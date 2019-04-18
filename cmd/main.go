5b0e17603eadf6d9d2779575b8e4f71f7bcbc2dc
paulfantom <pawel@krupa.net.pl>
2019-04-18 21:26:20 +0200

    init

---
+++ cmd/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"strconv"
+
+	mqttclient "github.com/automatedhome/flow-meter/pkg/mqttclient"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+type setpoints struct {
+	Vref float64 // cannot be 0 !
+	Tmin float64 // temperature on 0V
+	Tmax float64 // temperature on Vref
+}
+
+var publishTopic string
+var settings setpoints
+
+func onMessage(client mqtt.Client, message mqtt.Message) {
+	voltage, err := strconv.ParseFloat(string(message.Payload()), 64)
+	if err != nil {
+		log.Printf("Received incorrect message payload: '%v'\n", message.Payload())
+		return
+	}
+
+	temperature := calculate(voltage)
+	client.Publish(publishTopic, 0, false, fmt.Sprintf("%f", temperature))
+}
+
+func calculate(voltage float64) float64 {
+	// normalize voltage
+	temperature := (settings.Tmax-settings.Tmin)*voltage/settings.Vref + settings.Tmin
+	return temperature
+}
+
+func main() {
+	broker := flag.String("broker", "tcp://127.0.0.1:1883", "The full url of the MQTT server to connect to ex: tcp://127.0.0.1:1883")
+	clientID := flag.String("clientid", "analog-temperature", "A clientid for the connection")
+	inTopic := flag.String("inTopic", "evok/ai/1/value", "MQTT topic with a current analog value state")
+	outTopic := flag.String("outTopic", "solar/temperature/up", "MQTT topic to post a calculated temperature")
+	flag.Parse()
+
+	settings.Vref = 12.0
+	settings.Tmin = 0
+	settings.Tmax = 200
+	publishTopic = *outTopic
+	brokerURL, err := url.Parse(*broker)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mqttclient.New(*clientID, brokerURL, []string{*inTopic}, onMessage)
+
+	log.Printf("Connected to %s as %s and waiting for messages\n", *broker, *clientID)
+
+	// wait forever
+	select {}
+}
