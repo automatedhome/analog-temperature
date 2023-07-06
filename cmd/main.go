41fd255450c6069c47de501c07a5d38889e106e1
paulfantom <pawel@krupa.net.pl>
2023-07-06 08:16:01 +0200

    add prometheus metrics

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 	"strconv"
 
 	mqttclient "github.com/automatedhome/common/pkg/mqttclient"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type setpoints struct {
@@ -17,8 +21,25 @@ type setpoints struct {
 	Tmax float64 // temperature on Vref
 }
 
-var publishTopic string
-var settings setpoints
+var (
+	publishTopic string
+	settings     setpoints
+	tempMetric   = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "analog_sensor_temperature",
+		Help: "Temperature from analog sensor",
+		//ConstLabels: map[string]string{
+		//	"sensor": "ai1",  //TODO: this should be populated from user provided flags
+		//},
+	})
+	voltMetric = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "analog_sensor_voltage",
+		Help: "Voltage from analog sensor",
+	})
+	msgReceived = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "analog_sensor_messages_received",
+		Help: "The total number of messages received",
+	})
+)
 
 func onMessage(client mqtt.Client, message mqtt.Message) {
 	voltage, err := strconv.ParseFloat(string(message.Payload()), 64)
@@ -26,9 +47,18 @@ func onMessage(client mqtt.Client, message mqtt.Message) {
 		log.Printf("Received incorrect message payload: '%v'\n", message.Payload())
 		return
 	}
+	voltMetric.Set(voltage)
+
+	msgReceived.Inc()
 
 	temperature := calculate(voltage)
-	if err := mqttclient.Publish(client, publishTopic, 0, false, fmt.Sprintf("%.2f", temperature)); err != nil {
+	tempMetric.Set(temperature)
+	send(client, temperature)
+}
+
+func send(client mqtt.Client, temperature float64) {
+	topic := publishTopic
+	if err := mqttclient.Publish(client, topic, 0, false, fmt.Sprintf("%.2f", temperature)); err != nil {
 		log.Fatalln("MQTT message couldn't be sent. Exiting.")
 	}
 }
@@ -46,6 +76,9 @@ func main() {
 	outTopic := flag.String("outTopic", "solar/temperature/up", "MQTT topic to post a calculated temperature")
 	flag.Parse()
 
+	// Expose metrics
+	http.Handle("/metrics", promhttp.Handler())
+
 	settings.Vref = 12.0
 	settings.Tmin = 0   // input is at 0V (minimum)
 	settings.Tmax = 200 // input is at 10V (hardware allowed maximum)
@@ -57,6 +90,12 @@ func main() {
 	mqttclient.New(*clientID, brokerURL, []string{*inTopic}, onMessage)
 
 	log.Printf("Connected to %s as %s and waiting for messages\n", *broker, *clientID)
+
+	go func() {
+		if err := http.ListenAndServe(":7005", nil); err != nil {
+			panic("HTTP Server failed: " + err.Error())
+		}
+	}()
 
 	// wait forever
 	select {}
